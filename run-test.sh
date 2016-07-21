bdf4adb034bd1f195fb8c83367740897495ea8b1
gongzi <gongzi@GZ-JSQ-JP-BI-OPS-001.jp>
2016-07-21 09:46:34 +0800

    在ops上运行git mvn scp

---
+++ run-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd ~/dev_pro/dw-realtime/realtime-sourcedata/
+
+git pull
+
+mvn clean package
+
+scp ~/dev_pro/dw-realtime/realtime-sourcedata/target/realtime-souredata-1.0-SNAPSHOT-jar-with-dependencies.jar hadoop@spark001.jp:/home/hadoop/users/gongzi/
+
+if test $? -ne 0
+then
+echo "spark failed!"
+exit 2
+fi
