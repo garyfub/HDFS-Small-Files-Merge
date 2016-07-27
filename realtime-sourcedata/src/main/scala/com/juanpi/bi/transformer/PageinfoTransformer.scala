cc6c746a63ea85c9b7070fe35396be368348cfc3
gongzi <gongzi@juanpi.com>
2016-07-27 12:53:56 +0800

    case class 也需要定义变量

--- realtime-sourcedata/src/main/scala/com/juanpi/bi/transformer/PageinfoTransformer.scala
+++ realtime-sourcedata/src/main/scala/com/juanpi/bi/transformer/PageinfoTransformer.scala
@@ -11,7 +11,7 @@ import scala.collection.mutable
   */
 class PageinfoTransformer extends ITransformer {
 
-  def parse(row: JsValue, dimpage: mutable.HashMap[String, (Int, Int, String, Int)]): (User.type, PageAndEvent.type, Page.type, Event.type) = {
+  def parse(row: JsValue, dimpage: mutable.HashMap[String, (Int, Int, String, Int)]): (User, PageAndEvent, Page, Event) = {
     // mb_pageinfo
     val ticks = (row \ "ticks").asOpt[String].getOrElse("")
     val session_id = (row \ "session_id").asOpt[String].getOrElse("")
@@ -121,18 +121,11 @@ class PageinfoTransformer extends ITransformer {
     println("======>> page_id :: " + page_id)
     val (date, hour) = DateUtils.dateHourStr(endtime.toLong)
 
-//    Array(terminal_id,app_version,gu_id,utm,site_id,ref_site_id,uid,session_id,deviceid,page_id,
-//      page_value,ref_page_id,ref_page_value,page_level_id,page_lvl2_value,ref_page_lvl2_value,jpk,pit_type,sortdate,
-//      sorthour,lplid,ptplid,gid,ugroup,shop_id,ref_shop_id,starttime,endtime,hot_goods_id,ctag,location,ip,url,urlref,
-//      to_switch,parsed_source,event_id,event_value,rule_id,test_id,select_id,event_lvl2_value,loadTime,gu_create_time,tab_source
-//      ,date,hour
-//    ).mkString("\u0001")
-
-    User.apply(gu_id, utm, gu_create_time, session_id, terminal_id, app_version, site_id, ref_site_id, ctag)
-    PageAndEvent.apply(page_id, page_value, ref_page_id, ref_page_value, page_level_id, starttime, endtime, hot_goods_id, page_lvl2_value, ref_page_lvl2_value, jpk, pit_type, sortdate, sorthour, lplid, ptplid, gid, ugroup, table_source)
-    Page.apply(source, ip, url,urlref,deviceid,to_switch)
-    Event.apply(event_id,event_value,event_lvl2_value,rule_id,test_id,select_id)
-    (User, PageAndEvent, Page, Event)
+    val user = User.apply(gu_id, utm, gu_create_time, session_id, terminal_id, app_version, site_id, ref_site_id, ctag)
+    val pe = PageAndEvent.apply(page_id, page_value, ref_page_id, ref_page_value, page_level_id, starttime, endtime, hot_goods_id, page_lvl2_value, ref_page_lvl2_value, jpk, pit_type, sortdate, sorthour, lplid, ptplid, gid, ugroup, table_source)
+    val page = Page.apply(source, ip, url,urlref,deviceid,to_switch)
+    val event = Event.apply(event_id,event_value,event_lvl2_value,rule_id,test_id,select_id)
+    (user, pe, page, event)
   }
 
   // 返回解析的结果
@@ -160,11 +153,22 @@ class PageinfoTransformer extends ITransformer {
 
 // for test
 object PageinfoTransformer{
+
+
+  def testCaseClazz(): (User, Page, Event) = {
+    val u = User("1", "1", "1", "1", 0, "1", 0, 0, "1")
+    val p = Page("2", "", "", "", "", "")
+    val e = Event("3", "", "", "", "", "")
+
+    (u, p, e)
+  }
+
   def main(args: Array[String]) {
 
+    val pp = new PageinfoTransformer
     val liuliang =
       """
-        |{"app_name":"zhe","app_version":"3.4.6","c_label":"C3","c_server":"{\"gid\":\"C3\",\"ugroup\":\"143_223_112_142\"}","deviceid":"867568022962029","endtime":"1468929132822","endtime_origin":"1468929131796","extend_params":" {\n  "uid" : "36371591",\n  "validate" : 1\n}","gj_ext_params":"past_zhe,1580540_1500762_13504152,past_zhe,crazy_zhe","gj_page_names":"page_tab,page_home_brand_in,page_tab,page_tab","ip":"119.109.179.179","jpid":"ffffffff-bc21-7da8-ffff-ffffe4de7969","location":"辽宁省","os":"android","os_version":"4.4.4","pagename":"page_tab","pre_extend_params":"past_zhe","pre_page":"page_tab","server_jsonstr":"{\"ab_info\":{\"rule_id\":\"\",\"test_id\":\"\",\"select\":\"\"},\"ab_attr\":\"7\"}","session_id":"1468155168409_zhe_1468929047609","source":"","starttime":"1468929130209","starttime_origin":"1468929129183","ticks":"1468155168409","to_switch":"0","uid":"40102432","utm":"104954","wap_pre_url":"","wap_url":""}
+        |{"app_name":"zhe","app_version":"3.4.6","c_label":"C3","c_server":"{\"gid\":\"C3\",\"ugroup\":\"143_223_112_142\"}","deviceid":"867568022962029","endtime":"1468929132822","endtime_origin":"1468929131796","extend_params":"1","gj_ext_params":"past_zhe,1580540_1500762_13504152,past_zhe,crazy_zhe","gj_page_names":"page_tab,page_home_brand_in,page_tab,page_tab","ip":"119.109.179.179","jpid":"ffffffff-bc21-7da8-ffff-ffffe4de7969","location":"辽宁省","os":"android","os_version":"4.4.4","pagename":"page_tab","pre_extend_params":"past_zhe","pre_page":"page_tab","server_jsonstr":"{\"ab_info\":{\"rule_id\":\"\",\"test_id\":\"\",\"select\":\"\"},\"ab_attr\":\"7\"}","session_id":"1468155168409_zhe_1468929047609","source":"","starttime":"1468929130209","starttime_origin":"1468929129183","ticks":"1468155168409","to_switch":"0","uid":"40102432","utm":"104954","wap_pre_url":"","wap_url":""}
         |""".stripMargin
 
 
@@ -172,9 +176,11 @@ object PageinfoTransformer{
     println(pl)
 
     try{
-
       val line = Json.parse(pl)
-      println(line)
+//      val dp = new mutable.HashMap[String, (Int, Int, String, Int)]
+      val (user:User, page: Page, event: Event) = testCaseClazz()
+      println(user.site_id)
+      println(page.source)
     }catch{
       //使用模式匹配来处理异常
       case ex:IllegalArgumentException=>println(ex.getMessage())
@@ -182,11 +188,5 @@ object PageinfoTransformer{
       case ex:StringIndexOutOfBoundsException=>println("Invalid Index")
       case ex:Exception => println(ex.getStackTraceString, "\n======>>异常数据:" + pl)
     }
-
-    val aa = (Json.parse("{}") \ "order_status").asOpt[String].getOrElse("")
-    println(aa)
-
-    println(if("a_b".split("_").length > 2) {"a_b_c".split("_")(2)})
-
   }
 }
