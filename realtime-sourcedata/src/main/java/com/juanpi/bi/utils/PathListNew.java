69bb6822e49b37ef2d6d9bd18d73f0549a120ca8
gongzi <gongzi@juanpi.com>
2016-08-02 18:02:53 +0800

    test path

--- realtime-sourcedata/src/main/java/com/juanpi/bi/utils/PathListNew.java
+++ realtime-sourcedata/src/main/java/com/juanpi/bi/utils/PathListNew.java
@@ -27,7 +27,7 @@ import static org.apache.hadoop.io.WritableComparator.readVLong;
 public class PathListNew {
 
 //    static final Path INPUT_PATH = new Path("hdfs://nameservice1/user/hadoop/gongzi/dw_real_for_path_list/date=2016-07-30/gu_hash=0/");
-    static final String INPUT_PATH = "hdfs://nameservice1/user/hadoop/gongzi/dw_real_for_path_list/date=2016-07-30/gu_hash=0/";
+    static final String INPUT_PATH = "/user/hadoop/gongzi/dw_real_for_path_list/date=2016-07-30/gu_hash=0/";
     static final String OUT_PATH = "/user/hadoop/gongzi/dw_real_path_list/date=2016-07-30/gu_hash=0/";
 
     public static void main(String[] args) throws Exception {
