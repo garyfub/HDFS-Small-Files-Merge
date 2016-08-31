aebc795e1d235c9d3c88b549bbceda7f6903e4ba
gongzi <gongzi@juanpi.com>
2016-08-31 14:51:15 +0800

    test

--- pathlist/src/main/java/com/juanpi/bi/mapred/PathListControledJobs.java
+++ pathlist/src/main/java/com/juanpi/bi/mapred/PathListControledJobs.java
@@ -10,8 +10,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
@@ -31,7 +29,7 @@ import static org.apache.hadoop.io.WritableComparator.readVLong;
  * 烈烈
  * Created by kaenr on 2016/7/13.
  */
-public class NewPathList {
+public class PathListControledJobs {
 
     static String base = "hdfs://nameservice1/user/hadoop/gongzi";
 
@@ -48,7 +46,7 @@ public class NewPathList {
         try {
             fs = FileSystem.get(new Path(basePath).toUri(), conf);
             // 清理待存放数据的目录
-            if (fs.exists(new Path(outPath))) {
+            if(fs.exists(new Path(outPath))){
                 fs.delete(new Path(outPath), true);
             }
         } catch (IOException e) {
@@ -59,18 +57,18 @@ public class NewPathList {
 
     /**
      * 参考 http://bijian1013.iteye.com/blog/2306763
-     *
      * @return
      */
-    public static String getDateStr() {
+    public static String getDateStr()
+    {
         Calendar c1 = Calendar.getInstance();
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
         return format.format(c1.getTime());
     }
 
-    public static void JobsControl(String dateStr) {
+    public static void JobsControl(String dateStr){
 
-        if (dateStr == null || dateStr.isEmpty()) {
+        if(dateStr== null || dateStr.isEmpty()){
             dateStr = getDateStr();
         }
 
@@ -80,7 +78,7 @@ public class NewPathList {
         JobControl jc = new JobControl("mult-job");
 
         // 遍历16个分区
-        for (int i = 0x0; i <= 0xf; i++) {
+        for(int i=0x0; i<=0xf; i++) {
             String gu = String.format("%x", i);
 
             String str = "{0}/{1}/date={2}/gu_hash={3}/";
@@ -101,44 +99,41 @@ public class NewPathList {
             //将受控作业添加到控制器中
             //添加控制job
             try {
-                Job job = jobConstructor(inputPath, outputPath, gu);
+                Job job = jobConstructor(inputPath, outputPath);
                 ControlledJob cj = new ControlledJob(conf);
-                if ("f".equals(gu)) {
-
-                }
                 cj.setJob(job);
                 jc.addJob(cj);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+
         Thread jcThread = new Thread(jc);
         jcThread.start();
 
-        while (true) {
-            if (jc.allFinished()) {
+        while(true){
+            if(jc.allFinished()){
                 System.out.println("16个目录的数据处理完毕！");
                 System.out.println(jc.getSuccessfulJobList());
                 jc.stop();
-                break;
             }
 
-            if (jc.getFailedJobList().size() > 0) {
+            if(jc.getFailedJobList().size() > 0){
                 System.out.println(jc.getFailedJobList());
                 jc.stop();
-                break;
             }
         }
     }
 
     /**
+     *
      * @param inputPath
      * @param outputPath
      * @throws Exception
      */
-    public static Job jobConstructor(String inputPath, String outputPath, String gu) throws Exception {
+    public static Job jobConstructor(String inputPath, String outputPath) throws Exception {
 
-        Job job = Job.getInstance(conf, "pathListMR_" + gu);
+        Job job = Job.getInstance(conf, "split");
 
         // !! http://stackoverflow.com/questions/21373550/class-not-found-exception-in-mapreduce-wordcount-job
 //        job.setJar("pathlist-1.0-SNAPSHOT-jar-with-dependencies.jar");
@@ -148,8 +143,6 @@ public class NewPathList {
         //1.1 指定输入文件路径
         FileInputFormat.setInputPaths(job, inputPath);
         job.setInputFormatClass(TextInputFormat.class);//指定哪个类用来格式化输入文件
-
-        // -- -- -- -- -- -- -- -- Map -- -- -- -- -- -- -- --
 
         //1.2指定自定义的Mapper类
         job.setMapperClass(MyMapper.class);
@@ -167,8 +160,6 @@ public class NewPathList {
         job.setGroupingComparatorClass(MyGroupingComparator.class);
         //1.5  TODO （可选）合并
 
-        // -- -- -- -- -- -- -- -- Reduce -- -- -- -- -- -- -- --
-
         //2.2 指定自定义的reduce类
         job.setReducerClass(MyReducer.class);
 
@@ -180,8 +171,7 @@ public class NewPathList {
         FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
         //设定输出文件的格式化类
-//        job.setOutputFormatClass(TextOutputFormat.class);
-        LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
         return job;
         //把代码提交给JobTracker执行
@@ -190,135 +180,74 @@ public class NewPathList {
 
     static class MyMapper extends Mapper<LongWritable, Text, NewK2, TextArrayWritable> {
         int xx = 0;
-
-        private MultipleOutputs mos;
-
-        @Override
-        protected void setup(Context context)
-                throws IOException, InterruptedException {
-            super.setup(context);
-            mos = new MultipleOutputs(context);
-        }
-
-        @Override
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        protected void map(LongWritable key, Text value, Context context) throws IOException ,InterruptedException {
 
             final String[] splited = value.toString().split("\u0001");
 
-            String gu_id = splited[0];
-            String gu = gu_id.substring(gu_id.length() - 1).toLowerCase();
-            String dateStr = splited[13];
-
             // gu_id 和starttime 作为联合主键
-
             final NewK2 k2 = new NewK2(splited[0], Long.parseLong(splited[22]));
 
             //page_level_id,page_id,page_value,page_lvl2_value,event_id,event_value,event_lvl2_value,starttime作为 联合value
 
             // page_level_id  对应的路径 line
             // 21 page_level_id; 15 page_id; 16 page_value; 25: page_lvl2_value; 34: event_id; 35: event_value; 36: event_lvl2_value; 22: starttime
-            String str[] = {splited[21], splited[15] + "\t" + splited[16] + "\t" + splited[25] + "\t" + splited[34] + "\t" + splited[35] + "\t" + splited[36] + "\t" + splited[22], value.toString().replace("\u0001", "\t")};
+            String str[] = {splited[21], splited[15]+"\t"+splited[16]+"\t"+splited[25]+"\t"+splited[34]+"\t"+splited[35]+"\t"+splited[36]+"\t"+splited[22], value.toString().replace("\u0001","\t")};
             final TextArrayWritable v2 = new TextArrayWritable(str);
 
-            xx++;
+            xx ++;
 
-            mos.write(k2, v2, generateFileName(gu, dateStr));
-        }
-
-        // hdfs://nameservice1/user/hadoop/gongzi/dw_real_path_list/date=2016-08-13/gu_hash=0
-        // 目录输出格式 date=2016-08-13/gu_hash=0
-        private String generateFileName(String gu_hash, String dateStr) {
-            return "date=" + dateStr + "/gu_hash=" + gu_hash;
-        }
-
-        @Override
-        protected void cleanup(Context context)
-                throws IOException, InterruptedException {
-            super.cleanup(context);
-            mos.close();
+            context.write(k2, v2);
         }
     }
 
     //static class NewValue
 
     static class MyReducer extends Reducer<NewK2, TextArrayWritable, Text, Text> {
-
-        //1. 定义MultipleOutputs类型变量
-        private MultipleOutputs mos;
-
-        @Override
-        protected void setup(Reducer.Context context)
-                throws IOException, InterruptedException {
-            super.setup(context);
-            mos = new MultipleOutputs(context);
-        }
-
-        protected void reduce(NewK2 k2, Iterable<TextArrayWritable> v2s, Context context) throws IOException, InterruptedException {
+        protected void reduce(NewK2 k2, Iterable<TextArrayWritable> v2s, Context context) throws IOException ,InterruptedException {
             //long min = Long.MAX_VALUE;
-            String initstr = "" + "\t" + "" + "\t" + "" + "\t" + "" + "\t" + "" + "\t" + "" + "\t" + "";
-
-            String gu_id = k2.first;
-
-            String gu = gu_id.substring(gu_id.length() - 1).toLowerCase();
-            Long timeSecond = k2.second;
+            String initstr = ""+"\t"+""+"\t"+""+"\t"+""+"\t"+""+"\t"+""+"\t"+"";
 
             for (TextArrayWritable v2 : v2s) {
                 String level1 = initstr;
                 String level2 = initstr;
                 String level3 = initstr;
                 String level4 = initstr;
-                if (Integer.parseInt(v2.toStrings()[0]) == 1) {
-                    level1 = v2.toStrings()[1];
+                if(Integer.parseInt(v2.toStrings()[0]) == 1){
+                    level1=v2.toStrings()[1];
                     level2 = initstr;
                     level3 = initstr;
                     level4 = initstr;
-                } else if (Integer.parseInt(v2.toStrings()[0]) == 2) {
-                    level2 = v2.toStrings()[1];
+                } else if(Integer.parseInt(v2.toStrings()[0]) == 2){
+                    level2=v2.toStrings()[1];
                     level3 = initstr;
                     level4 = initstr;
-                } else if (Integer.parseInt(v2.toStrings()[0]) == 3) {
+                } else if(Integer.parseInt(v2.toStrings()[0]) == 3){
                     level3 = v2.toStrings()[1];
                     level4 = initstr;
-                } else if (Integer.parseInt(v2.toStrings()[0]) == 4) {
+                } else if(Integer.parseInt(v2.toStrings()[0]) == 4){
                     level4 = v2.toStrings()[1];
                 }
 
                 // 4 个级别
-                Text key2 = new Text(level1 + "\t" + level2 + "\t" + level3 + "\t" + level4);
+                Text key2 = new Text(level1+"\t"+ level2+"\t"+level3+"\t"+level4);
                 Text value2 = new Text(v2.toStrings()[2]);
-//                context.write(key2, value2);
-
-                mos.write(key2, value2, generateFileName(gu, timeSecond));
+                context.write(key2, value2);
             }
-        }
 
-        // hdfs://nameservice1/user/hadoop/gongzi/dw_real_path_list/date=2016-08-13/gu_hash=0
-        // 目录输出格式 date=2016-08-13/gu_hash=0
-        private String generateFileName(String gu_hash, Long timeSecond) {
-            SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            String dateStr = mDateFormat.format(timeSecond);
-            return "date=" + dateStr + "/gu_hash=" + gu_hash;
-        }
-
-        @Override
-        protected void cleanup(Reducer.Context context)
-                throws IOException, InterruptedException {
-            super.cleanup(context);
-            mos.close();
         }
     }
 
     /**
-     * 原来的v2不能参与排序，把原来的k2和v2封装到一个类中，作为新的k2
+     原来的v2不能参与排序，把原来的k2和v2封装到一个类中，作为新的k2
+     *
      */
-    static class NewK2 implements WritableComparable<NewK2> {
+    static class  NewK2 implements WritableComparable<NewK2> {
         String first;
         Long second;
 
-        public NewK2() {
-        }
+        public NewK2(){}
 
-        public NewK2(String first, long second) {
+        public NewK2(String first, long second){
             this.first = first;
             this.second = second;
         }
@@ -343,24 +272,24 @@ public class NewPathList {
         @Override
         public int compareTo(NewK2 o) {
             final long minus = this.first.compareTo(o.first);
-            if (minus != 0) {
-                return (int) minus;
+            if(minus !=0){
+                return (int)minus;
             }
-            return (int) (this.second - o.second);
+            return (int)(this.second - o.second);
         }
 
         @Override
         public int hashCode() {
-            return this.first.hashCode() + this.second.hashCode();
+            return this.first.hashCode()+this.second.hashCode();
         }
 
         @Override
         public boolean equals(Object obj) {
-            if (!(obj instanceof NewK2)) {
+            if(!(obj instanceof NewK2)){
                 return false;
             }
-            NewK2 oK2 = (NewK2) obj;
-            return (this.first.equals(oK2.first)) && (this.second == oK2.second);
+            NewK2 oK2 = (NewK2)obj;
+            return (this.first.equals(oK2.first))&&(this.second==oK2.second);
         }
     }
 
@@ -368,7 +297,7 @@ public class NewPathList {
 
         @Override
         public int compare(NewK2 o1, NewK2 o2) {
-            return (int) (o1.first.compareTo(o2.first));
+            return (int)(o1.first.compareTo(o2.first));
         }
 
         @Override
@@ -416,12 +345,12 @@ public class NewPathList {
             set(texts);
         }
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String dateStr = args[0];
-        if (dateStr == null || dateStr.isEmpty()) {
+        if(dateStr== null || dateStr.isEmpty()){
             JobsControl("");
-        } else {
+        } else
+        {
             JobsControl(dateStr);
         }
     }
--- pathlist/src/main/java/com/juanpi/bi/mapred/PathListNew.java
+++ pathlist/src/main/java/com/juanpi/bi/mapred/PathListNew.java
@@ -200,19 +200,15 @@ public class PathListNew {
 
         //  throws IOException ,InterruptedException
         @Override
-        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        protected void map(LongWritable key, Text value, Context context){
 
             final String[] splited = value.toString().split("\001");
 
             System.out.println("======>>Oooooo: " + splited);
             NewK2 k2 = new NewK2("",0);
-            TextArrayWritable v2 = null;
-            String gu = "";
-
             try {
                 String gu_id = splited[0];
-                gu = gu_id.substring(gu_id.length() - 1).toLowerCase();
-                String dateStr = splited[13];
+                String gu = gu_id.substring(gu_id.length() - 1).toLowerCase();
 
                 // gu_id 和starttime 作为联合主键
 
@@ -221,26 +217,24 @@ public class PathListNew {
                 k2.second = Long.parseLong(splited[22]);
 
                 //page_level_id,page_id,page_value,page_lvl2_value,event_id,event_value,event_lvl2_value,starttime作为 联合value
-
                 // page_level_id  对应的路径 line 每一级别加上 loadtime
                 // 21 page_level_id; 15 page_id; 16 page_value; 25: page_lvl2_value; 34: event_id; 35: event_value; 36: event_lvl2_value; 22: starttime
                 String loadTime = splited[46];
                 String str[] = {splited[21],
                         splited[15]+"\t"+splited[16]+"\t"+splited[25]+"\t"+splited[34]+"\t"+splited[35]+"\t"+splited[36]+"\t"+splited[22] + "\t" + loadTime,
                         value.toString().replace("\001","\t")};
-                v2 = new TextArrayWritable(str);
+                final TextArrayWritable v2 = new TextArrayWritable(str);
 
                 xx ++;
 
-            }
-//            catch (IOException e)
-//            {
-//                e.printStackTrace();
-//            } catch (InterruptedException e)
-//            {
-//                e.printStackTrace();
-//            }
-            catch(ArrayIndexOutOfBoundsException | NumberFormatException | StringIndexOutOfBoundsException e)
+                mos.write(k2, v2, generateFileName(gu));
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            } catch(ArrayIndexOutOfBoundsException | NumberFormatException | StringIndexOutOfBoundsException e)
             {
                 e.printStackTrace();
                 System.out.println("======>>ArrayIndexOutOfBoundsException: " + value.toString());
@@ -251,8 +245,6 @@ public class PathListNew {
                 System.out.println("======>>ArrayIndexOutOfBoundsException: " + value.toString());
                 System.out.println("======>>ArrayIndexOutOfBoundsException: " + splited);
             }
-
-            mos.write(k2, v2, generateFileName(gu));
         }
 
         // hdfs://nameservice1/user/hadoop/gongzi/dw_real_path_list/date=2016-08-13/gu_hash=0
