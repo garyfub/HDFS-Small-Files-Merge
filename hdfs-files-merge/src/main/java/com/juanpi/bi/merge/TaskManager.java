65976e33161602e6bcd314fcf0c2d083ccd64939
gongzi <gongzi@juanpi.com>
2016-09-09 02:01:09 +0800

    test

--- hdfs-files-merge/src/main/java/com/juanpi/bi/merge/TaskManager.java
+++ hdfs-files-merge/src/main/java/com/juanpi/bi/merge/TaskManager.java
@@ -26,7 +26,7 @@ public class TaskManager {
 	// 路径正则
 	//	
 	private String getDirRegex() {
-		return baseDir + "/date=" + date + "/gu_hash=" + gu_hash + "/";
+		return baseDir + "/date=" + date + "/*/";
 	}
 	
 	/**
