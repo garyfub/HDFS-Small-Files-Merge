679a4985d68cd0d4c4ca3e13bf8ad40533c59017
gongzi <gongzi@juanpi.com>
2016-09-13 17:25:06 +0800

    主函数增加两个参数的判断。一个是日期。一个是数据目录

--- hdfs-files-merge/src/main/java/com/juanpi/bi/merge/TaskManager.java
+++ hdfs-files-merge/src/main/java/com/juanpi/bi/merge/TaskManager.java
@@ -15,42 +15,62 @@ import com.juanpi.bi.merge.util.ProcessUtil;
  */
 public class TaskManager {
 	
-	private String date = null;
-	private String gu_hash = null;
+
 	private String baseDir = null;
 
 
 	public TaskManager(String baseDir) {
 		this.baseDir = baseDir;
-		this.date = ProcessUtil.getBeforeOneHourDate();	// yyyy-mm-dd
-		this.gu_hash = "*"; // HH
 	}
 
 	
 	// 路径正则 TODO 测试 2016-09-09 号数据
-	private String getDirRegex() {
-		return baseDir + "mb_event_hash2/date=" + "2016-09-09" + "/gu_hash={a}/";
+	private String getDirRegex(String dateStr) {
+		return baseDir + "mb_event_hash2/date=" + dateStr + "/gu_hash={a}/";
 	}
 	
 	/**
 	 * 开始任务
 	 * @throws IOException
 	 */
-	public void start() throws IOException {
+	public void start(String dateStr) throws IOException {
         System.out.println("开始任务======>>....");
-        String srcDirRegex = getDirRegex();
+        String srcDirRegex = getDirRegex(dateStr);
 		MergeTask mergeTask = new MergeTask(srcDirRegex, null, true);
 		mergeTask.doMerge();
 	}
 	
 	public static void main(String[] args) {
-        String dir = "hdfs://nameservice1/user/hadoop/gongzi/dw_real_for_path_list/";
-        System.out.println("======>>main_dir:" + dir);
+
+		// 传入 date 格式 yyyy-MM-dd
+        // 传入 hdfs 目录
+
+        String dateStr = "";
+        String dir = "";
+
+		if(args.length > 1)
+        {
+            dateStr = args[0];
+            dir = args[1];
+        }
+
+        if(null == dateStr || dateStr.isEmpty())
+		{
+            dateStr = ProcessUtil.getBeforeOneHourDate();
+		}
+
+        if(null == dir || dir.isEmpty())
+        {
+            dir = "hdfs://nameservice1/user/hadoop/gongzi/dw_real_for_path_list/";
+        }
+
+        System.out.println("======>>main_date:" + dateStr);
+        System.out.println("======>>main_dir :" + dir);
+
 		TaskManager manager = new TaskManager(dir);
 		try {
-			manager.start();
+			manager.start(dateStr);
 		} catch (Exception e) {
-            System.out.println(e.getStackTrace());
             e.printStackTrace();
 			System.exit(-1);
 		}
