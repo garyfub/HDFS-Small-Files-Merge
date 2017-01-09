e5a0b36815cd5c9f47d1ddf827f7d4709c8cd090
gongzi <gongzi@juanpi.com>
2017-01-09 18:58:52 +0800

    冗余部分代码

--- pathlist/src/main/java/com/juanpi/bi/mapred/PathListControledJobs.java
+++ pathlist/src/main/java/com/juanpi/bi/mapred/PathListControledJobs.java
@@ -196,11 +196,11 @@ public class PathListControledJobs {
                     //page_level_id,page_id,page_value,page_lvl2_value,event_id,event_value,event_lvl2_value,starttime作为 联合value
                     // page_level_id  对应的路径 line
                     // 21 page_level_id; 15 page_id; 16 page_value; 25: page_lvl2_value; 34: event_id; 40: event_value; 36: event_lvl2_value; 22: starttime
-                    String page_level_id = (splited[21] == null)? "\\N":splited[21];
-                    String page_id = (splited[15] == null) ? "\\N":splited[15];
+                    String pageLevelId = (splited[21] == null)? "\\N":splited[21];
+                    String pageId = (splited[15] == null) ? "\\N":splited[15];
                     String page_value = (splited[16] == null) ? "\\N":splited[16];
                     String page_lvl2_value = (splited[25] == null) ? "\\N":splited[25];
-                    String event_id = (splited[40] == null) ? "\\N":splited[40];
+                    String eventId = (splited[40] == null) ? "\\N":splited[40];
                     String event_value = (splited[41] == null) ? "\\N":splited[41];
                     String event_lvl2_value = (splited[42] == null) ? "\\N":splited[42];
                     String startTime = (splited[22] == null) ? "\\N":splited[22];
@@ -216,14 +216,33 @@ public class PathListControledJobs {
                     String ug_id  = (splited[47] == null) ? "\\N":splited[47];
 
                     // 推荐点击为入口页(购物袋页、品牌页、商祥页底部)
-                    String pageLvlId = CommonLogic.getPageLevelId(page_level_id, page_id, event_id);
+                    String pageLvlId = pageLevelId;
+
+                    // 推荐点击为入口页(购物袋页、品牌页、商祥页底部)
+                    if("481".equals(eventId) || "10041".equals(eventId)){
+                        if("158".equals(pageId) || "167".equals(pageId) || "250".equals(pageId) || "26".equals(pageId)) {
+                            pageLvlId = "1";
+                        }
+                    } else if("10043".equals(eventId)){
+                        if("10084".equals(pageId) || "10085".equals(pageId)){
+                            pageLvlId = "5";
+                        }
+                    } else if("10050".equals(eventId)){
+                        if("10085".equals(pageId) || "10107".equals(pageId)){
+                            pageLvlId = "5";
+                        }
+                    } else if("448".equals(eventId)){
+                        if("158".equals(pageId)){
+                            pageLvlId = "5";
+                        }
+                    }
 
                     String str[] = {
                             pageLvlId,
-                            page_id
+                            pageId
                             + "\t" + page_value
                             + "\t" + page_lvl2_value
-                            + "\t" + event_id
+                            + "\t" + eventId
                             + "\t" + event_value
                             + "\t" + event_lvl2_value
                             + "\t" + startTime
