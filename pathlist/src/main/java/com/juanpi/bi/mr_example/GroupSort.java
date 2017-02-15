863980f9a691adcfede497febca09b157487cd81
gongzi <gongzi@juanpi.com>
2017-02-15 09:13:31 +0800

    remove unused iport

--- pathlist/src/main/java/com/juanpi/bi/mr_example/GroupSort.java
+++ pathlist/src/main/java/com/juanpi/bi/mr_example/GroupSort.java
@@ -15,7 +15,6 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
