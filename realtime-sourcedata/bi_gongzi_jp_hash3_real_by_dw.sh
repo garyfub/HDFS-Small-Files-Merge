2958d20da58a3d634f897e1e5eb093497baeac65
dianmao <dianmao@juanpi.com>
2017-02-15 11:51:13 +0800

    测试h5page

--- realtime-sourcedata/bi_gongzi_jp_hash3_real_by_dw.sh
+++ realtime-sourcedata/bi_gongzi_jp_hash3_real_by_dw.sh
@@ -14,7 +14,7 @@ echo "gongzi com.juanpi.bi.streaming.KafkaConsumer jp_hash3 start..."
     --total-executor-cores 12 \
     --conf "spark.default.parallelism=24" \
     --driver-java-options "-XX:PermSize=1024M -XX:MaxPermSize=3072M -Xmx4096M -Xms2048M -Xmn1024M" \
-    *.jar "zkQuorum"="GZ-JSQ-JP-BI-KAFKA-001.jp:2181,GZ-JSQ-JP-BI-KAFKA-002.jp:2181,GZ-JSQ-JP-BI-KAFKA-003.jp:2181,GZ-JSQ-JP-BI-KAFKA-004.jp:2181,GZ-JSQ-JP-BI-KAFKA-005.jp:2181" "brokerList"="kafka-broker-000.jp:9082,kafka-broker-001.jp:9083,kafka-broker-002.jp:9084,kafka-broker-003.jp:9085,kafka-broker-004.jp:9086,kafka-broker-005.jp:9087,kafka-broker-006.jp:9092,kafka-broker-007.jp:9093,kafka-broker-008.jp:9094,kafka-broker-009.jp:9095,kafka-broker-010.jp:9096,kafka-broker-011.jp:9097" "topic"="jp_hash3" "groupId"="bi_gongzi_jp_hash3_real_by_dw" "consumerType"=1 "consumerTime"=60 "maxRecords"=100
+    *.jar "zkQuorum"="GZ-JSQ-JP-BI-KAFKA-001.jp:2181,GZ-JSQ-JP-BI-KAFKA-002.jp:2181,GZ-JSQ-JP-BI-KAFKA-003.jp:2181,GZ-JSQ-JP-BI-KAFKA-004.jp:2181,GZ-JSQ-JP-BI-KAFKA-005.jp:2181" "brokerList"="kafka-broker-000.jp:9082,kafka-broker-001.jp:9083,kafka-broker-002.jp:9084,kafka-broker-003.jp:9085,kafka-broker-004.jp:9086,kafka-broker-005.jp:9087,kafka-broker-006.jp:9092,kafka-broker-007.jp:9093,kafka-broker-008.jp:9094,kafka-broker-009.jp:9095,kafka-broker-010.jp:9096,kafka-broker-011.jp:9097" "topic"="jp_hash3" "groupId"="bi_dianmao_jp_hash3_real_by_dw_test" "consumerType"=1 "consumerTime"=60 "maxRecords"=100
 
 if test $? -ne 0
 then
