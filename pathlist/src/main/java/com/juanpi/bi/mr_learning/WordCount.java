932599508e42762a68e1981fe78bb53e2245d61a
dianmao <dianmao@juanpi.com>
2017-03-07 15:46:31 +0800

    wordcount代码编写完成，本地打包测试

--- pathlist/src/main/java/com/juanpi/bi/mr_learning/WordCount.java
+++ pathlist/src/main/java/com/juanpi/bi/mr_learning/WordCount.java
@@ -5,11 +5,12 @@ package com.juanpi.bi.mr_learning;
  */
 
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.text.MessageFormat;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,18 +20,24 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
-    public static class MapTest extends Mapper<Object, Text, Text, IntWritable>{
+    private static String getInputPath(String dateStr) {
+        String inputPath = "hdfs://nameservice1/user/hive/warehouse/dw.db/fct_page_ref/date=" +dateStr;
+        return inputPath;
+    }
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+    private static String getOutputPath(String dateStr) {
+        String outputPath = "hdfs://nameservice1/user/dianmao/wordcount/";
+        return outputPath;
+    }
 
-        public void map(Object key, Text value, Context context
+    public static class MapTest extends Mapper<LongWritable, Text, String, IntWritable>{
+
+        public void map(LongWritable key, Text value, Context context
         ) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
+            IntWritable one = new IntWritable(1);
+            final String[] splited = value.toString().split("\001");
+            final String key1 = splited[0];
+                context.write(key1, one);
         }
     }
 
@@ -58,8 +65,8 @@ public class WordCount {
         job.setReducerClass(com.juanpi.bi.mr_learning.WordCount.ReduceTest.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(getInputPath(args[0])));
+        FileOutputFormat.setOutputPath(job, new Path(getInputPath(args[0])));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
